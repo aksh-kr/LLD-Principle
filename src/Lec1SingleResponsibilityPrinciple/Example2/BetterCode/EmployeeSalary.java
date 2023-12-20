cd561fa1d824daa9ab96264f40731b66b4f7a221
Akash Kumar <kr.akash149@gmail.com>
2023-12-20 12:21:59 +0530

    changed author

--- src/Lec1SingleResponsibilityPrinciple/Example2/BetterCode/EmployeeSalary.java
+++
@@ -1,7 +0,0 @@
-package Lec1SingleResponsibilityPrinciple.Example2.BetterCode;
-
-public class EmployeeSalary {
-    public double calculateSalary(){
-        return 0;
-    }
-}
--- src/Lec1SingleResponsibilityPrinciple/Example2/BetterCode/FetchBioData.java
+++
@@ -1,6 +0,0 @@
-package Lec1SingleResponsibilityPrinciple.Example2.BetterCode;
-
-public class FetchBioData {
-
-    public
-}
