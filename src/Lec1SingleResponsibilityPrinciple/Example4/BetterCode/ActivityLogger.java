6abd9ad0b95b6044fcb9dba2f8c2a07ae826d2c2
Akash Kumar <kr.akash149@gmail.com>
2023-12-20 12:55:05 +0530

    Example 4

---
+++ src/Lec1SingleResponsibilityPrinciple/Example4/BetterCode/ActivityLogger.java
@@ -0,0 +1,7 @@
+package src.Lec1SingleResponsibilityPrinciple.Example4.BetterCode;
+
+public class ActivityLogger {
+    public void logUserActivity(String username) {
+        // Code to log user's activity
+    }
+}
---
+++ src/Lec1SingleResponsibilityPrinciple/Example4/BetterCode/User.java
@@ -0,0 +1,14 @@
+package src.Lec1SingleResponsibilityPrinciple.Example4.BetterCode;
+
+public class User {
+    // User repository userRepo
+    public void createUser(String username) {
+        // Code to create a user
+        // this.userRepo.create(...)
+    }
+
+    public void deleteUser(String username) {
+        // Code to delete a user
+        // this.userRepo.destroy(...)
+    }
+}
